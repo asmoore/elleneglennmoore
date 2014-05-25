6dc4fa06fc1bd1be38f06f6f96f8c3531dd63701
Andrew Moore <asmoor@gmail.com>
2014-05-25 18:54:20 -0400

    add email pass to osenviron

--- elleneglennmoore/settings.py
+++ elleneglennmoore/settings.py
@@ -151,7 +151,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'asmoor@gmail.com'
-#EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASSWORD']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASSWORD']
 EMAIL_PORT = 587
 
 # A sample logging configuration. The only tangible logging
